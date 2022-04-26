86e42410b1ae6df75ee3fa6453fc37146e5bc7d9
Md. Siam <md.siam03@gmail.com>
2022-04-26 06:44:53 +0600

    firebase authentication done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-import 'src/firebase/signin_page.dart';
 import 'src/views/home_page.dart';
 
 void main() async {
