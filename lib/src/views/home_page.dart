7878a0eb2a57fa6f807308396ab78dca2c16375f
Md. Siam <md.siam03@gmail.com>
2022-04-29 21:27:00 +0600

    minor bug fixed

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -18,7 +18,7 @@ class _HomePageState extends State<HomePage> {
   late VideoPlayerController _controller1;
   late VideoPlayerController _controller2;
   late VideoPlayerController _controller3;
-  final sc_controller = ScrollController();
+  final scrollController = ScrollController();
 
   @override
   void initState() {
@@ -67,29 +67,29 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          controller: sc_controller,
+          controller: scrollController,
           child: Column(
             children: [
               const CustomAppBar(),
               PlayerWidget(
                 videoNum: videoList[0].videoNumber!.toInt(),
                 videoController: _controller0,
-                scrollController: sc_controller,
+                scrollController: scrollController,
               ),
               PlayerWidget(
                 videoNum: videoList[1].videoNumber!.toInt(),
                 videoController: _controller1,
-                scrollController: sc_controller,
+                scrollController: scrollController,
               ),
               PlayerWidget(
                 videoNum: videoList[2].videoNumber!.toInt(),
                 videoController: _controller2,
-                scrollController: sc_controller,
+                scrollController: scrollController,
               ),
               PlayerWidget(
                 videoNum: videoList[3].videoNumber!.toInt(),
                 videoController: _controller3,
-                scrollController: sc_controller,
+                scrollController: scrollController,
               ),
               const CustomBottomBar()
             ],
