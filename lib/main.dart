108f9e0ae7d175916d5cb9048ea327aa53f45a67
Md. Siam <md.siam03@gmail.com>
2022-05-05 17:40:53 +0600

    LICENSE file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Md. Siam
+// http://mdsiam.xyz/
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://choosealicense.com/licenses/mit/
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
